edeb3d55b1d53e520b1c2bff6d6fb943bd6f126b
eugenioenko <eugenioenko@gmail.com>
2019-04-30 00:06:59 -0400

    fixed editor

--- src/firenice.js
+++ src/firenice.js
@@ -1,5 +1,8 @@
 import { Game } from './game';
+import { Consts } from './constants';
 import { Resources } from './resources';
+import { Fire } from './fire';
+import { Jar } from './jar';
 
 window.addEventListener('load', () => {
     const resources = new Resources();
@@ -24,4 +27,56 @@ window.addEventListener('load', () => {
             e.target.blur();
         }
     });
+
+    if (FIRENICE_EDITOR_MODE) {
+        game.engine.sound.musicOn = false;
+		game.engine.sound.soundOn = false;
+        game.state = Consts.GAME_STATE_PLAY;
+        game.engine.editor = true;
+        game.engine.keyboard.intro = false;
+
+        canvas.addEventListener('click', (e) => {
+            const xtile = Math.floor(e.offsetX / 32);
+            const ytile = Math.floor(e.offsetY / 32);
+            if (tool < 2) {
+                game.engine.map.map[ytile][xtile] = tool;
+            } else {
+                switch (tool) {
+                    case Consts.OBJECT_PLAYER:
+                        game.engine.player.x = xtile * 32;
+                        game.engine.player.y = ytile * 32;
+                        break;
+                    case Consts.OBJECT_ICE:
+                        game.engine.addIceBlock(xtile, ytile);
+                        break;
+                    case Consts.OBJECT_METAL:
+                        game.engine.addSprite(new Metal(game.engine, xtile, ytile, 1));
+                        break;
+                    case Consts.OBJECT_FIRE:
+                        game.engine.addSprite(new Fire(game.engine, xtile, ytile));
+                        break;
+                    case Consts.OBJECT_JAR:
+                        game.engine.addSprite(new Jar(game.engine, xtile, ytile));
+                        break;
+                }
+            }
+        });
+
+        document.getElementById('theme-selector').addEventListener('change', (e) => {
+            game.engine.map.theme = parseInt(e.target.value, 10);
+            e.target.blur();
+        });
+
+        document.getElementById('level-selector').addEventListener('change', (e) => {
+            if (e.target.value !== '-1') {
+                game.engine.level = parseInt(e.target.value, 10);
+                game.engine.scene.load(game.engine.level);
+                e.target.blur();
+            }
+        });
+
+        document.getElementById('btn-save').addEventListener('click', () => {
+            document.getElementById('txt-level').value = JSON.stringify(game.engine.scene.save());
+        });
+    }
 });
