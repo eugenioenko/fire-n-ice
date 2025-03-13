ee86e9fc74c3e73fdb33c0cc516767272493a18c
Eugene Yakhnenko <eugenioenko@gmail.com>
2025-03-12 21:47:24 -0700

    fix: audio sounds preloading

--- src/firenice.js
+++ src/firenice.js
@@ -6,22 +6,28 @@ import { Metal } from './metal';
 import { Resources } from './resources';
 import { Teleport } from './teleport';
 
-window.addEventListener('load', () => {
-  const loader = document.getElementById('loader');
-
-  function onStartClick() {
-    loader.style.display = 'none';
-    loader.style.opacity = 0;
-    PreloadGame();
-    window.firenice = true;
-  }
+window.addEventListener('load', async () => {
   if (window.FIRENICE_EDITOR_MODE) {
-    onStartClick();
+    doStartClick();
   }
-  loader.addEventListener('click', () => onStartClick());
+  loader.addEventListener('click', () => doStartClick());
 });
 
-function PreloadGame() {
+async function doStartClick() {
+  const loader = document.getElementById('loader');
+  loader.style.display = 'none';
+  loader.style.opacity = 0;
+  const resources = await loadGameResources();
+  startGame(resources);
+  if (window.FIRENICE_EDITOR_MODE) {
+    loadGameEditor();
+  }
+  window.firenice = true;
+}
+
+async function loadGameResources() {
+  let resolve;
+  const promise = new Promise(res => (resolve = res));
   const resources = new Resources();
   resources.add('image', 'tilemap', 'images/tilemap.png');
   resources.add('image', 'img_ice', 'images/ice.png');
@@ -47,9 +53,10 @@ function PreloadGame() {
   resources.add('audio', 'sfx-music-sparks', 'music/sparks.mp3');
 
   resources.ready(() => {
-    StartGame(resources);
-    CheckEditor();
+    resolve(resources);
   });
+
+  return promise;
 }
 
 function loadLevelFromEvent(event) {
@@ -58,7 +65,7 @@ function loadLevelFromEvent(event) {
   game.engine.scene.load(game.engine.level);
 }
 
-function StartGame(resources) {
+function startGame(resources) {
   let canvas = document.getElementById('canvas');
   let game = new Game(canvas, resources);
   window.game = game;
@@ -69,52 +76,50 @@ function StartGame(resources) {
   lvlSelector.style.opacity = 1;
 }
 
-function CheckEditor() {
-  if (window.FIRENICE_EDITOR_MODE) {
-    game.engine.sound.musicOn = false;
-    game.engine.sound.soundOn = false;
-    game.state = Consts.GameStatePlay;
-    game.engine.editor = true;
-    game.engine.keyboard.intro = false;
-    game.engine.sound.music.pause();
+function loadGameEditor() {
+  game.engine.sound.musicOn = false;
+  game.engine.sound.soundOn = false;
+  game.state = Consts.GameStatePlay;
+  game.engine.editor = true;
+  game.engine.keyboard.intro = false;
+  game.engine.sound.music.pause();
 
-    canvas.addEventListener('click', e => {
-      const xTile = Math.floor(e.offsetX / 32);
-      const yTile = Math.floor(e.offsetY / 32);
-      if (tool < 2) {
-        game.engine.map.map[yTile][xTile] = tool;
-      } else {
-        switch (tool) {
-          case Consts.ObjectPlayer:
-            game.engine.player.x = xTile * 32;
-            game.engine.player.y = yTile * 32;
-            break;
-          case Consts.ObjectIce:
-            game.engine.addIceBlock(xTile, yTile);
-            break;
-          case Consts.ObjectMetal:
-            game.engine.addSprite(new Metal(game.engine, xTile, yTile, 1));
-            break;
-          case Consts.ObjectFire:
-            game.engine.addSprite(new Fire(game.engine, xTile, yTile));
-            break;
-          case Consts.ObjectJar:
-            game.engine.addSprite(new Jar(game.engine, xTile, yTile));
-            break;
-          case Consts.ObjectTeleport:
-            game.engine.addSprite(new Teleport(game.engine, xTile, yTile));
-            break;
-        }
+  canvas.addEventListener('click', e => {
+    const xTile = Math.floor(e.offsetX / 32);
+    const yTile = Math.floor(e.offsetY / 32);
+    if (tool < 2) {
+      game.engine.map.map[yTile][xTile] = tool;
+    } else {
+      switch (tool) {
+        case Consts.ObjectPlayer:
+          game.engine.player.x = xTile * 32;
+          game.engine.player.y = yTile * 32;
+          break;
+        case Consts.ObjectIce:
+          game.engine.addIceBlock(xTile, yTile);
+          break;
+        case Consts.ObjectMetal:
+          game.engine.addSprite(new Metal(game.engine, xTile, yTile, 1));
+          break;
+        case Consts.ObjectFire:
+          game.engine.addSprite(new Fire(game.engine, xTile, yTile));
+          break;
+        case Consts.ObjectJar:
+          game.engine.addSprite(new Jar(game.engine, xTile, yTile));
+          break;
+        case Consts.ObjectTeleport:
+          game.engine.addSprite(new Teleport(game.engine, xTile, yTile));
+          break;
       }
-    });
+    }
+  });
 
-    document.getElementById('theme-selector').addEventListener('change', e => {
-      game.engine.map.theme = parseInt(e.target.value, 10);
-      e.target.blur();
-    });
+  document.getElementById('theme-selector').addEventListener('change', e => {
+    game.engine.map.theme = parseInt(e.target.value, 10);
+    e.target.blur();
+  });
 
-    document.getElementById('btn-save').addEventListener('click', () => {
-      document.getElementById('txt-level').value = JSON.stringify(game.engine.scene.save());
-    });
-  }
+  document.getElementById('btn-save').addEventListener('click', () => {
+    document.getElementById('txt-level').value = JSON.stringify(game.engine.scene.save());
+  });
 }
